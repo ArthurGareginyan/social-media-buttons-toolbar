7b1aa4b39837b47aaad1788413aeec4b96fc5c5c
Arthur Gareginyan <arthurgareginyan@gmail.com>
2015-12-17 02:45:17 +0300

    First commit. First stable release. Version 1.0.

---
+++ inc/settings_page.php
@@ -0,0 +1,238 @@
+ <?php
+
+/**
+ * Prevent Direct Access
+ */
+defined('ABSPATH') or die("Restricted access!");
+
+/**
+ * Render Settings Page
+ *
+ * @since 1.0
+ */
+function smbtoolbar_render_submenu_page() {
+
+	// Declare variables
+    $options = get_option( 'smbtoolbar_settings' );
+
+	// Settings update message
+	if ( isset( $_GET['settings-updated'] ) ) :
+		?>
+			<div id="message" class="updated">
+				<p>
+					<?php _e( 'Your options was sucessfully updated.', 'smbtoolbar' ); ?>
+				</p>
+			</div>
+		<?php
+	endif;
+
+	// Page
+	?>
+	<div class="wrap">
+		<h2>
+            <?php _e( 'Social Media Buttons Toolbar', 'smbtoolbar' ); ?>
+            <br/>
+            <span>
+                <?php _e( 'by <a href="http://mycyberuniverse.com/author.html" target="_blank">Arthur "Berserkr" Gareginyan</a>', 'smbtoolbar' ); ?>
+            <span/>
+		</h2>
+
+        <div id="poststuff" class="metabox-holder has-right-sidebar">
+
+            <!-- SIDEBAR -->
+            <div class="inner-sidebar">
+                <div id="side-sortables" class="meta-box-sortabless ui-sortable">
+
+                    <div id="about" class="postbox">
+                        <h3 class="title"><?php _e( 'About', 'smbtoolbar' ); ?></h3>
+                        <div class="inside">
+                            <p class="about">
+                                <img src="<?php echo plugins_url('thanks.png', __FILE__); ?>">
+                            </p>
+                            <p><?php _e( 'This plugin allows you to easily add the social media buttons toolbar to any place of your web-site.', 'smbtoolbar' ) ?></p>
+                            <p><?php _e( 'To use, enter your User ID\'s, then click "Save Changes". It\'s that simple!', 'smbtoolbar' ) ?></p>
+                        </div>
+                    </div>
+
+                    <div id="donate" class="postbox">
+                        <h3 class="title"><?php _e( 'Donate', 'smbtoolbar' ); ?></h3>
+                        <div class="inside">
+                            <p class="donate"><?php _e( 'If you like this plugin and find it useful, help me to make this plugin even better and keep it up-to-date.', 'smbtoolbar' ) ?></p>
+                            <div class="aligncenter">
+                                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8A88KC7TFF6CS" target="_blank" rel="nofollow">
+                                    <img src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" alt="Make a donation">
+                                </a>
+                            </div>
+                            <p class="donate"><?php _e( 'Thanks for your support!', 'smbtoolbar' ) ?></p>
+                        </div>
+                    </div>
+
+                    <div id="help" class="postbox">
+                        <h3 class="title"><?php _e( 'Help', 'smbtoolbar' ); ?></h3>
+                        <div class="inside">
+                            <div class="aligncenter">
+                                <p><?php _e( 'If you want more options then tell me and I will be happy to add it.', 'smbtoolbar' ); ?></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- END-SIDEBAR -->
+
+            <!-- FORM -->
+            <div class="has-sidebar sm-padded">
+                <div id="post-body-content" class="has-sidebar-content">
+                    <div class="meta-box-sortabless">
+
+                        <form name="smbtoolbar-form" action="options.php" method="post" enctype="multipart/form-data">
+                            <?php settings_fields( 'smbtoolbar_settings_group' ); ?>
+
+                            <div class="postbox" id="UserIDs">
+                                <h3 class="title"><?php _e( 'User ID’s', 'smbtoolbar' ); ?></h3>
+                                <div class="inside">
+                                    <p class="description"><?php _e( 'Enter your User ID for each service you choose below to make the button link directly to your profile. If you don\'t want to use any of the following options, you can just forget about these.', 'smbtoolbar' ); ?></p>
+                                    <table class="form-table">
+                                        <?php smbtoolbar_media( 'facebook',
+                                                                'Facebook',
+                                                                'https://www.facebook.com/arthur.gareginyan',
+                                                                'Enter the link to your Facebook profile page',
+                                                                'hhttps://www.facebook.com'
+                                        );?>
+                                        <?php smbtoolbar_media(  'twitter',
+                                                                'Twitter',
+                                                                'https://twitter.com/AGareginyan',
+                                                                'Enter the link to your Twitter profile page',
+                                                                'https://twitter.com'
+                                        );?>
+                                        <?php smbtoolbar_media( 'instagram',
+                                                                'Instagram',
+                                                                'http://instagram.com/arthur_gareginyan/',
+                                                                'Enter the link to your Instagram profile page',
+                                                                'http://instagram.com'
+                                        );?>
+                                        <?php smbtoolbar_media( 'google-plus',
+                                                                'Google+',
+                                                                'https://plus.google.com/u/0/+ArthurGareginyan/',
+                                                                'Enter the link to your Google+ profile page',
+                                                                'https://plus.google.com'
+                                        );?>
+                                        <?php smbtoolbar_media( 'youtube',
+                                                                'YouTube',
+                                                                'https://www.youtube.com/channel/UCvQenE1DumnZy1k5sTvgmSA',
+                                                                'Enter the link to your YouTube profile page',
+                                                                'https://www.youtube.com'
+                                        );?>
+                                        <?php smbtoolbar_media( 'blogger',
+                                                                'Blogger',
+                                                                'http://oneberserk.blogspot.com',
+                                                                'Enter the link to your Blogger profile page',
+                                                                'https://www.blogger.com'
+                                        );?>
+                                        <?php smbtoolbar_media( 'linkedin',
+                                                                'LinkedIn',
+                                                                'https://linkedin.com/in/arthurgareginyan',
+                                                                'Enter the link to your LinkedIn profile page',
+                                                                'https://linkedin.com'
+                                        );?>
+                                        <?php smbtoolbar_media( 'github',
+                                                                'Github',
+                                                                'https://github.com/ArthurGareginyan',
+                                                                'Enter the link to your Github profile page',
+                                                                'https://github.com'
+                                        );?>
+                                        <?php smbtoolbar_media( 'wordpress',
+                                                                'WordPress',
+                                                                'https://profiles.wordpress.org/arthur-gareginyan/',
+                                                                'Enter the link to your WordPress profile page',
+                                                                'https://wordpress.org'
+                                        );?>
+                                        <?php smbtoolbar_media( 'codepen',
+                                                                'Codepen',
+                                                                'http://codepen.io/berserkr/',
+                                                                'Enter the link to your Codepen profile page',
+                                                                'http://codepen.io'
+                                        );?>
+                                        <?php smbtoolbar_media( 'email',
+                                                                'Email',
+                                                                'arthurgareginyan@gmail.com',
+                                                                'Enter your email address',
+                                                                ''
+                                        );?>
+                                        <?php smbtoolbar_media( 'rss-feed',
+                                                                'RSS Feed',
+                                                                'http://arthurgareginyan.com/feed',
+                                                                'Enter the link to your RSS Fedd',
+                                                                ''
+                                        );?>
+                                    </table>
+                                    <?php submit_button( __( 'Save Changes', 'smbtoolbar' ), 'primary', 'submit', true ); ?>
+                                </div>
+                            </div>
+
+                            <div class="postbox" id="DisplayOptions">
+                                <h3 class="title"><?php _e( 'Display options', 'smbtoolbar' ) ?></h3>
+                                <div class="inside">
+                                    <p class="description"></p>
+                                    <table class="form-table">
+                                        <?php smbtoolbar_setting('show_posts',
+                                                                  'Show on Posts',
+                                                                  'Display toolbar below content on Posts',
+                                                                  'check'
+                                        );?>
+                                        <?php smbtoolbar_setting('show_pages',
+                                                                  'Show on Pages',
+                                                                  'Display toolbar below content on Pages',
+                                                                  'check'
+                                        );?>
+                                        <?php smbtoolbar_setting('new_tab',
+                                                                  'Open link in new tab/window',
+                                                                  '',
+                                                                  'check'
+                                        );?>
+                                        <?php smbtoolbar_setting('icon-size',
+                                                                  'Icon size',
+                                                                  'Enter the size of icons (in px) in your social media buttons toolbar.',
+                                                                  'field',
+                                                                  '64',
+                                                                  '2'
+                                        );?>
+                                        <?php smbtoolbar_setting('margin-right',
+                                                                  'Margin right',
+                                                                  'Enter the size of space (in px) between icons in your social media buttons toolbar.',
+                                                                  'field',
+                                                                  '10',
+                                                                  '2'
+                                        );?>
+                                        <?php smbtoolbar_setting('caption',
+                                                                  'Caption',
+                                                                  'Enter the caption to your social media buttons toolbar. It will be displays before the toolbar.',
+                                                                  'field',
+                                                                  'Follow me on social media:',
+                                                                  '50'
+                                        );?>
+                                    </table>
+                                    <?php submit_button( __( 'Save Changes', 'smbtoolbar' ), 'primary', 'submit', true ); ?>
+                                </div>
+                            </div>
+
+                            <div class="postbox" id="Preview">
+                                <h3 class="title"><?php _e( 'Preview', 'smbtoolbar' ) ?></h3>
+                                <div class="inside">
+                                    <p class="description"><?php _e( 'Enter your User ID\'s, then click  "Save Changes" to update this preview.', 'smbtoolbar' ) ?></p></br>
+                                    <?php echo smbtoolbar_shortcode(); ?>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- END-FORM -->
+
+            </form>
+
+        </div>
+
+	</div>
+	<?php
+}
---
+++ readme.txt
@@ -0,0 +1,147 @@
+=== Social Media Buttons Toolbar ===
+Contributors: Arthur Gareginyan
+Tags: custom, simple, plugin, facebook, twitter, instagram, g+, google+, google plus, google-plus, google_plus, googleplus, youtube, blogger, linkedin, github, wordpress, codepen, email, mail, rss, rss feed, rss-feed, rss_feed, rssfeed, feed, icon, icon set, iconset, social, button, social button, social-button, social-buttons-2, social button 2,
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8A88KC7TFF6CS
+Requires at least: 3.9
+Tested up to: 4.4
+Stable tag: 1.0
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+...
+
+== Description ==
+...
+
+
+= Features =
+
+**Current features:**
+
+* Icon set [Social Buttons 2](https://www.iconfinder.com/iconsets/social-buttons-2) by Ivlichev Victor Petrovich
+* Ready for translation (.pot file included).
+* Added Russian translation.
+
+** Buttons **
+
+* Facebook
+* Twitter
+* Instagram
+* Google+
+* YouTube
+* Blogger
+* LinkedIn
+* GitHub
+* WordPress
+* Codepen
+* Email
+* RSS Feed
+
+**Coming soon:**
+
+* Pinterest
+* VKontakte
+* Odnoklassniki
+* Personal web-site
+* (Got more ideas? Tell me!)
+
+...
+
+
+For more details please visit - [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-social-media-buttons-toolbar.html)
+
+**Please Vote and Enjoy**
+
+* Your votes really make a difference! Thanks.
+
+== Installation ==
+Install "Social Media Buttons Toolbar" just as you would any other WordPress Plugin.
+
+Automatically via WordPress:
+
+1. Log into WordPress Dashboard of your website.
+2. Go to "`Plugins`" —> "`add new plugins`".
+3. Find this plugin and click install.
+4. Activate this plugin through the "`Plugins`" tab.
+
+Manual via FTP:
+
+1. Download a copy (zip file) of this plugin from WordPress.org.
+2. Unzip the zip file.
+3. Upload the unzipped directory to your website's plugin directory (`/wp-content/plugins/`).
+4. Log into WordPress Dashboard of your website.
+5. Activate this plugin through the "`Plugins`" tab.
+
+After installation, a "`Social Buttons`" menu item will appear in the "`Settings`" section. Click on this in order to view plugin's administration page.
+
+[More help installing Plugins](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins "WordPress Codex: Installing Plugins")
+
+
+== Frequently Asked Questions ==
+= Will this Plugin work on my WordPress.com website? =
+Sorry, this plugin is available for use only on self-hosted (WordPress.org) websites.
+
+= Can I use this plugin on my language? =
+Yes. But If your language is not available then you can make one. This plugin is ready for translation. The `.pot` file is included and placed in "`languages`" folder. Many of plugin users would be delighted if you shared your translation with the community. Just send the translation files (`*.po, *.mo`) to me at the arthurgareginyan@gmail.com and I will include the translation within the next plugin update.
+
+= How does it work? =
+First, go to page "`Settings`" —> "`Social Buttons`".
+On the plugins page you find the fields where you can enter your `User ID's`, `URL` to your social network profile.
+
+= Where to report bug if found? =
+Please visit [Dedicated Plugin Page on GitHub](https://github.com/ArthurGareginyan/social-media-buttons-toolbar) and report.
+
+= Where to share any ideas or suggestions to make the plugin better? =
+Please visit [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-social-media-buttons-toolbar.html) and share.
+
+
+== Screenshots ==
+1. Plugin’s page. Section "User ID's".
+2. Plugin’s page. Section "Display Options".
+3. Plugin’s page. Section "Preview".
+4. Social Media Buttons Toolbar displayed below the content of a post (Twenty Fifteen theme).
+5. Social Media Buttons Toolbar displayed in the sidebar using a shortcode in text widget (Twenty Fifteen theme).
+6. Social Media Buttons Toolbar displayed in the footer using a simple call the function.
+
+== Other Notes ==
+
+"Social Media Buttons Toolbar" is one of the personal software projects of Arthur Gareginyan.
+
+**License**
+
+This plugin is open-sourced software licensed under the [GNU General Public License, version 3 (GPLv3)](http://www.gnu.org/licenses/gpl-3.0.html) and is distributed free of charge.
+Commercial licensing (e.g. for projects that can’t use an open-source license) is available upon request.
+
+**Icons**
+
+Icon set [Social Buttons 2](https://www.iconfinder.com/iconsets/social-buttons-2) by Ivlichev Victor Petrovich is licensed under the Creative Commons (Attribution 3.0 Unported).
+
+**Support**
+
+* Did you enjoy this plugin? Please [donate to support ongoing development](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8A88KC7TFF6CS). Your contribution would be greatly appreciated.
+* Do you have any ideas or suggestions to make the plugin better? I can’t wait to hear them! Please share! [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-social-media-buttons-toolbar.html)
+
+**Please Vote and Enjoy**
+
+* Your votes really make a difference! Thanks.
+
+**Links**
+
+* [Developer Website](http://mycyberuniverse.com)
+* [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-social-media-buttons-toolbar.html)
+* [Dedicated Plugin Page on GitHub](https://github.com/ArthurGareginyan/social-media-buttons-toolbar)
+
+== Changelog ==
+= 1.0 =
+* Initial release.
+* Added ready for translation (.pot file included).
+* Added Russian translation.
+= 0.3 =
+* Release candidate.
+= 0.2 =
+* Beta version.
+= 0.1 =
+* Alfa version.
+
+
+== Upgrade Notice ==

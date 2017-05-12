efa9ad8f9fbf8c2f869a25e10e81b0dca97b825a
Arthur Gareginyan <arthurgareginyan@gmail.com>
2017-05-12 22:02:35 +0300

    Plugin updated to version 4.3.1

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: icon, icon set, button, social, media, social button, social media, social
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8A88KC7TFF6CS
 Requires at least: 3.9
 Tested up to: 4.7
-Stable tag: 4.3
+Stable tag: 4.3.1
 License: GPL3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -199,8 +199,11 @@ Commercial licensing (e.g. for projects that can’t use an open-source license)
 
 == Changelog ==
 
+= 4.3.1 =
+* Fixed the bug due to which the the "Warning: Illegal string offset 'version' in" and the "Warning: Illegal string offset 'old_version' in" warnings are displayed. (Thanks to Sven Brill)
+
 = 4.3 =
-* Added option for the diaspora button.
+* Added option for the diaspora social network.
 
 = 4.2 =
 * The design of the plugin settings page is completely redone.
--- social-media-buttons-toolbar.php
+++ social-media-buttons-toolbar.php
@@ -5,7 +5,7 @@
  * Description: Easily add the smart bar with social media follow buttons (not share, only link to your profiles) to any place of your WordPress website.
  * Author: Arthur Gareginyan
  * Author URI: http://www.arthurgareginyan.com
- * Version: 4.3
+ * Version: 4.3.1
  * License: GPL3
  * Text Domain: social-media-buttons-toolbar
  * Domain Path: /languages/
@@ -62,7 +62,7 @@ defined('SMEDIABT_BASE') or define('SMEDIABT_BASE', plugin_basename(__FILE__));
 defined('SMEDIABT_URL') or define('SMEDIABT_URL', plugin_dir_url(__FILE__));
 defined('SMEDIABT_PATH') or define('SMEDIABT_PATH', plugin_dir_path(__FILE__));
 defined('SMEDIABT_TEXT') or define('SMEDIABT_TEXT', 'social-media-buttons-toolbar');
-defined('SMEDIABT_VERSION') or define('SMEDIABT_VERSION', '4.3');
+defined('SMEDIABT_VERSION') or define('SMEDIABT_VERSION', '4.3.1');
 
 /**
  * Load the plugin modules

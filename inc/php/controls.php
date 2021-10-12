cf1598aa626a642bb9a837a48788a090a89bf782
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-10-12 23:03:04 +0300

    Maintenance: Processing of options has been improved

--- inc/php/controls.php
+++ inc/php/controls.php
@@ -158,7 +158,7 @@ function spacexchimp_p005_control_link( $name, $label, $placeholder, $help, $lin
 
     // Put the value of the plugin options into an array for easier access
     $options = spacexchimp_p005_options();
-    $option = !empty( $options['buttons-link'][$name] ) ? esc_url( $options['buttons-link'][$name] ) : '';
+    $option = $options['buttons-link'][$name];
     $display = !empty( $options['buttons-selected'][$name] ) ? '' : 'none';
     $disabled = !empty( $options['buttons-selected'][$name] ) ? '' : 'disabled'; // Required to bypass the limitation of the PHP directive max_input_vars=1000. This allows to avoid storing empty options.
 
